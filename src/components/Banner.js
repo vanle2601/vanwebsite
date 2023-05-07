39c0cc7a2567c2489bc7cb2438d762c740eca9e5
vanle2601 <89347635+vanle2601@users.noreply.github.com>
2023-05-07 14:44:58 +1000

    add feature sites

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/bunno.png";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
 const Banner = () => {
   return (
@@ -8,22 +7,19 @@ const Banner = () => {
       <Container>
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-            
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>Hi, my name is Van.</h1>
-                  <p>
-                    A front end developer with an addiction to discovering new coding perspectives. I really mean it!
-                  </p>
-                  <button href="#skills">Let's Dive In <ArrowRightCircle size={25} /></button>
-              
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img"/>
+            <h1>Hi, my name is Van.</h1>
+            <p>
+              A front end developer with an addiction to discovering new coding
+              perspectives. I really mean it!
+            </p>
+            <button href="#skills">
+              Let's Dive In <ArrowRightCircle size={25} />
+            </button>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
 
 export default Banner;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,104 +1,59 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
+import BuildingCard from "./BuildingCard";
 
-import projImg1 from "../assets/img/gymbunno.png";
-import projImg2 from "../assets/img/fonseka.png";
-import projImg3 from "../assets/img/vanportfolio.png";
-import projImg4 from "../assets/img/fakeflix.png";
-import colorSharp from "../assets/img/color-sharp.png"
-
-const Projects = () => {
-
-    const buildingProjects = [
-        {
-        title: "Streaming Platform",
-        description: "API-based",
-        imgUrl: projImg4,
-        link: "https://fakeflix-clone.netlify.app/",
-        },
-    ];
-
-    const projects = [
-        {
-        title: "Search Engine",
-        description: "API-based",
-        imgUrl: projImg1,
-        link: "https://gymbunny.netlify.app/",
-        },
-        {
-        title: "Landing Page",
-        description: "Design & Development",
-        imgUrl: projImg2,
-        link: "",
-        },
-        {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-        link: "https://vvnle.com",
-        },
-    ];
-
+const Projects = ({ projects, buildingProjects }) => {
   return (
     <section className="project" id="project">
       <Container>
         <Row>
-            <Col size={12}>
-                <h2>Projects</h2>
-                <p>
-                  As a passionate and insatiably curious learner, 
-                  I thrive on the rush of excitement and challenge that comes with tackling new projects.
-                </p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Launched</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Building</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
+          <Col size={12}>
+            <h2>Projects</h2>
+            <p>
+              As a passionate and insatiably curious learner, I thrive on the
+              rush of excitement and challenge that comes with tackling new
+              projects.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Launched</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Building</Nav.Link>
+                </Nav.Item>
+              </Nav>
 
-                  <Tab.Content>
-                  <Tab.Pane eventKey="second">
-                    <Row>
-                        {
-                          buildingProjects.map((buildingProject, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...buildingProject}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
+              <Tab.Content>
+                <Tab.Pane eventKey="second">
+                  <Row>
+                    {buildingProjects.map((buildingProject) => (
+                      <BuildingCard
+                        buildingProject={buildingProject}
+                        key={buildingProject.id}
+                      />
+                    ))}
+                  </Row>
                 </Tab.Pane>
-                  
+
                 <Tab.Pane eventKey="first">
-                    <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                    </Row>
+                  <Row>
+                    {projects.map((project) => (
+                      <ProjectCard project={project} key={project.id} />
+                    ))}
+                  </Row>
                 </Tab.Pane>
-                    
-                </Tab.Content>
-                
-                </Tab.Container>
-            </Col>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src={colorSharp} alt="bg-img" />
     </section>
-  )
-}
+  );
+};
 
 export default Projects;
